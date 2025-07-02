dce323ac308c378f5517288d87d0a7e70bed7c4f
iadesignerweb <appcifrinha@gmail.com>
2025-07-02 20:23:07 -0300

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-function toggleService(el) {
-  document.querySelectorAll('.service-description').forEach(d => d.style.display = 'none');
-  const desc = el.querySelector('.service-description');
-  desc.style.display = desc.style.display === 'block' ? 'none' : 'block';
-}
